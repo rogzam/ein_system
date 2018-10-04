dac0bebfc78a2e629f76515530376c6ad03f8640
rogzam <rogzam@gmail.com>
2018-10-04 18:55:30 +0200

    Script now captures counts and writes properly

--- poc_cou/hog_captures.py
+++ poc_cou/hog_captures.py
@@ -1,4 +1,4 @@
-### BASIC HOG PEOPLE COUNTER / RZ - 2018 ###
+### CAPTURER AND HOG COUNTER / RZ - 2018 ###
 
 ## LIBRARIES
 
@@ -8,69 +8,89 @@ import cv2
 import datetime
 import time
 
-## CAPTURE IMAGE
-
-vid_cap = cv2.VideoCapture('foo_sam/foo_sam_01.MOV');
-
-count = 0
-
-def rsz(image,fac=2):
-    '''Resizes an image by the factor indicated in both width and height'''
-    h, w = image.shape[:2]
-    img_rsz = cv2.resize(image,(int(w/2),int(h/2)))
-    return img_rsz
-
-img_ini = rsz(vid_cap.read()[1],2)
-
-img_a = vid_cap.read()[1]
-print('read a new frame:')
-
-img_rsz = rsz(img_a,2)
-
-cv2.imwrite('pho_sam/frame.jpg',img_a)
-print('success')
-
-count+=1
-cv2.imshow('Holder', img_ini)
-
-vid_cap.release()
-
-## IMAGE ANALIZED
-
 ## DEVICE ID
 
 cou_id = '0001'
 
 ## DEFINITIONS
 
-tim_now = datetime.datetime.now().strftime("%Y%m%d_%H%M")
+foo_src = 'foo_sam/foo_sam_01.MOV'
+img_des = 'pho_pro/'
+
+cap_fmt = '.png'
+cyc_int = 200
+cyc_cnt = 0
+
+rsz_fct = 3
 
 hog_des = cv2.HOGDescriptor()
 hog_des.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
-img_nam = 'pho_sam/frame.jpg'
+## FUNCTIONS
 
-img_raw = cv2.imread(img_nam)
-height, width = img_raw.shape[:2]
+def rsz_img(img,fct):   
+    '''Resizes an image by the factor indicated in both width and height'''
+        
+    h, w = img.shape[:2]
+    img_rsz = cv2.resize(img,(int(w/fct),int(h/fct)))
+    return img_rsz
 
-img_gry = cv2.cvtColor(img_raw, cv2.COLOR_BGR2GRAY)
+def src_id(src):
+    '''Takes the video source and assigns a string name in case the footage
+       comes from the webcam directory'''
+    
+    if type(src) == int:
+        src = 'CAMERA {}'.format(str(src))
+        return src
+    else:
+        return src
 
-img_rsz = img_raw
-#img_rsz = cv2.resize(img_raw,(int(width/2),int(height/2)))
+def hog_img(src):
+    '''Takes an image detects and counts full-body shapes. Returns both the image and
+       an integer with the number of pedestrians counted.'''
+    
+    (rects, weights) = hog_des.detectMultiScale(src, winStride=(4, 4), padding=(8, 8), scale=1.05)
 
+    rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
+    pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+
+    for (xA, yA, xB, yB) in pick:
+        cv2.rectangle(src, (xA, yA), (xB, yB), (0, 0, 200), 2)
+
+    cnt = len(rects)
+
+    print("{} PEDESTRIANS DETECTED IN IMAGE '{}'.".format(cnt,img_nam))
+    
+    return src, cnt
+
+def nam_img(fmt='.png'):
+    '''Produces a name for the capture based on a timestamp and an image format defined above'''
+    
+    tim_now = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    nam = '{}_[{}]{}'.format(tim_now,cou_id,fmt)
+    return nam
+    
 ## EXECUTION
 
-(rects, weights) = hog_des.detectMultiScale(img_rsz, winStride=(4, 4), padding=(8, 8), scale=1.05)
+vid_cap = cv2.VideoCapture(foo_src);
 
-rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
-pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+while True:
 
-for (xA, yA, xB, yB) in pick:
-        cv2.rectangle(img_rsz, (xA, yA), (xB, yB), (0, 0, 200), 2)
+    _, img_raw = vid_cap.read()
 
-print("{} PEDESTRIANS DETECTED IN IMAGE '{}'.".format(len(rects),img_nam))
-cv2.imshow("hog_basic", img_rsz)
-cv2.imwrite('pho_pro/{}_[{}].png'.format(tim_now,cou_id),img_rsz)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    if cyc_cnt % cyc_int == 0:
+               
+        img_rsz = rsz_img(img_raw,rsz_fct)
+        img_nam = nam_img(cap_fmt)
+        img_hog, cnt_hog = hog_img(img_rsz)
 
+        cv2.imwrite(img_des+img_nam, img_rsz)
+        
+        print('IMAGE CAPTURED FROM {} AND RESIZED BY {}.'.format(foo_src,rsz_fct))
+
+    else:
+        pass
+    #print('EMPTY CYCLE')
+
+    cyc_cnt+=1
+    
